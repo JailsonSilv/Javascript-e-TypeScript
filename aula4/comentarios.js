d6fb92973e82bdbc120508fdc650ee1a0630ea38
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-04 11:44:57 -0300

    Subindo os estudos em javascript

---
+++ aula4/comentarios.js
@@ -0,0 +1,13 @@
+// Meu comentario 
+
+// console.log("Olá Mundo");
+
+console.log("Linha1") //'Linha será exibido.
+/* 
+   Tudo que estiver entre aqui dentro não será exibido.
+console.log("Linha2")
+*/
+
+let gaveta = '10 Camisas'//'Linha será exibido.
+console.log(gaveta);//'Linha será exibido.
+
