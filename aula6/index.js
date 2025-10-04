f29814e7af9928353a2ff9d96704956b6d443c38
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-04 15:56:12 -0300

    Aprendendo criar variaveis e da valor a elas

---
+++ aula6/index.js
@@ -0,0 +1,19 @@
+/*  Primeiro exemplo.
+let nome = "Lucas";
+console.log(nome , `nasceu em 1984.`);
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve um filho com",nome,"em 2015.");
+console.log("O filho de", nome,"se chama Eduardo.");
+*/
+
+let nome;
+nome = "Lucas"; //String
+console.log(nome);
+
+
+let nomeCliente ="Caio";
+let idadeCliente = "45 Anos";
+
+console.log("O melhor cliente do mês foi o:",nomeCliente);
+console.log(nomeCliente,"tem",idadeCliente);
