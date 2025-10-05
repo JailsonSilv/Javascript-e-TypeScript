52716d43a89440eddf3b43b5ce2533c6e6ee0d12
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-05 12:32:25 -0300

    Aprendendo os conceitos da CONST e template strings

---
+++ aula7/index.js
@@ -0,0 +1,4 @@
+const primeiroNum = 10;
+const segundoNum = 3;
+
+console.log(primeiroNum + segundoNum);
---
+++ aula8/index.js
@@ -0,0 +1,14 @@
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const alturaEmM = 1.8;
+let indiceMassaCorporal; // peso / (altura * altura)
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+let anoNascimento;
+anoNascimento = 2025 - idade;
+
+
+console.log(`${nome} ${sobrenome} tem, ${idade} anos e pesa ${peso} kg`);
+console.log(`Tem ${alturaEmM} de altura, e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento} .`);
