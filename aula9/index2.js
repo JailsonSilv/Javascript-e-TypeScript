8e460426584499c1e4415b42e4d3eb6bdab18099
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-05 13:43:56 -0300

    Tipos de dados primitivos em javascript

---
+++ aula9/index2.js
@@ -0,0 +1,12 @@
+const nome = 'Jailson'; // string
+const nome1 = "Jajá"; // string
+const nome2 = `Silva`; // string
+const num1 = 7; //Number
+const num2 = 7.7; //Number
+let nomeAluno; // Apenas declarei mas não inicializei. Nesse caso essa fica UNDEFINED.
+const sobrenomeAluno = null; // Não aponta pra lugar nenhum na memoria.
+const aprovado = false; // Boolean = true, false (lógico).
+
+
+
+console.log(typeof sobrenomeAluno, sobrenomeAluno)
