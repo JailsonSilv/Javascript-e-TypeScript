16f01184cb9c46d2db30c952fa6b95e8ff09df34
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-05 12:55:50 -0300

    Diferença entre VAR e LET

---
+++ aula9/index.js
@@ -0,0 +1,19 @@
+var nome = "Lucas";
+var nome = "Gabriel";
+
+console.log(nome2);
+
+/* Em VAR toda vez que eu declarar uma variavel por exemplo NOME e tentar declarar outra variavel(VAR)
+vou estar redefinindo ela, como podemos ver no exemplo acima. 
+=> Primeiro 
+var nome = "Lucas";
+
+=> Segundo
+var nome = "Gabriel";
+
+No console, foi exibido  <<<< Gabriel >>>>
+
+
+Já com LET alertaria um ERRO por declaração.
+*/
+
