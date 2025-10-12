48cf15f4b990246f5fc83620dc051db1ea0252f4
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-12 16:21:17 -0300

    Aprendendo o basico do Array

---
+++ aula15/index.js
@@ -0,0 +1,36 @@
+// As strings são indexadas 01234567
+// const nome = 'Jailson'
+// console.log(nome[2])
+
+// Os arrays tb.    0         1       2
+let alunos = ['Beatriz', 'Lucas', 'Ana'];
+alunos = 123;
+
+
+//delete alunos[1];  -> deletando mais o index permanece
+
+// const alunosExpuso = alunos.pop(); -> REMOVE DO FINAL 
+
+// const alunoRemovido = alunos.shift();
+// console.log(alunoRemovido)
+console.log( typeof alunos)
+console.log(alunos instanceof Array)
+
+//alunos.unshift('Fabiana'); // adiciona no começo
+//alunos.unshift('Diego');
+
+//alunos.push('Bruno');
+//alunos.push('Henrique');
+//alunos.push('Gustavo')
+
+//alunos[alunos.length] = 'Luiza';
+//alunos[alunos.length] = 'Fabio';
+//alunos[alunos.length] = 'Barbara';
+
+//Alterando
+//alunos[0] = 'Gabriel'
+// Criando
+//alunos[3] = 'Luiz'
+
+
+// console.log(alunos[2])
