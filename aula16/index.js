65d3e07b026777bea73dad9e92935ed660ea3f20
Jailson Silva <jailsonsilvaoto@gmail.com>
2025-10-12 17:16:12 -0300

    Aprendendo funcões

---
+++ aula16/index.js
@@ -0,0 +1,29 @@
+function saudacao(nome) {
+  console.log(`Bom Dia ${nome}!!!`)
+}
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+
+//saudacao("Lucas");
+//saudacao("Lara");
+//saudacao("Luan");
+
+/*
+function multiplicacao (x, y) {
+  const result = x * y;
+  return result;
+}
+
+console.log(multiplicacao(2, 10));
+
+
+function soma(a, b) {
+  const resultado = a + b;
+  return resultado;
+}
+
+console.log(soma(10, 10));
+
+*/
